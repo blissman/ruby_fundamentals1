2edd3c3a94fa545f228c88c807bec9e8ee433fb2
Bliss Man <blissman@gmail.com>
2016-01-12 14:34:58 -0500

    missed the year part for ex3; added now

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name?"
 username = gets.chomp
 puts "Hi #{username}!"
+
+puts "Now, how old are you turning this year?"
+age = gets.chomp
+puts "Your were born in #{2016-age.to_i}."
