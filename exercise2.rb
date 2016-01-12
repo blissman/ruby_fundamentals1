dd8624c3f31352e3c020b93537603ac62436bae7
Bliss Man <blissman@gmail.com>
2016-01-12 13:53:14 -0500

    added question 4 (nailed the prediction)

--- exercise2.rb
+++ exercise2.rb
@@ -3,3 +3,10 @@ puts "$#{55*1.2}"
 
 #Question 2
 puts "Let's add this string to " + 4.to_s
+
+#Question 3
+puts "The product of 45628 and 7839 is #{45628 * 7839}."
+
+#Question 4
+#Predict True
+puts (10 < 20 && 30 < 20) || !(10 == 11)
